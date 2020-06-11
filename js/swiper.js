b73190d3a0dd135262af4a29af3cfdb6b3d9adc1
AndriiMSN <59262899+AndriiMSN@users.noreply.github.com>
2020-06-11 21:49:41 +0300

    add cert

--- js/swiper.js
+++ js/swiper.js
@@ -1,46 +1,54 @@
 var sliders = [];
 
-$(".swiper-container").each(function(index, element){
+$(".swiper-container").each(function (index, element) {
   var $this = $(this);
   $this.addClass("instance-" + index);
   $this.find(".swiper-button-prev").addClass("btn-prev-" + index);
   $this.find(".swiper-button-next").addClass("btn-next-" + index);
   console.log(index);
   var swiper = new Swiper(".instance-" + index, {
-    slidesPerView:1,
+    slidesPerView: 1,
     spaceBetween: 30,
     observer: true,
     observeParents: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
     navigation: {
       nextEl: '.btn-next-' + index,
       prevEl: '.btn-prev-' + index,
     },
-    breakpoints:{
-      0:{
-        slidesPerView:1,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
         spaceBetween: 0
       },
-      320:{
-        slidesPerView:1,
+      320: {
+        slidesPerView: 1,
         spaceBetween: 0
       },
-      575:{
-        slidesPerView:1
+      575: {
+        slidesPerView: 1
       },
 
-      768:{
-        slidesPerView:1,
+      768: {
+        slidesPerView: 1,
         spaceBetween: 10
       },
-      992:{
-        slidesPerView:1,
+      992: {
+        slidesPerView: 1,
         spaceBetween: 10
       },
-      1300:{
-        slidesPerView:1,
+      1300: {
+        slidesPerView: 1,
         spaceBetween: 0
       }
     }
-    
+
   });
 });
